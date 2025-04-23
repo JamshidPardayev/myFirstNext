afd647ad95e3132651052ac44942678193a5e6b2
JamshidPardayev <jamshidnt2024@gmail.com>
2025-04-23 16:12:57 +0500

    Postlar Qushildi

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Post = async ({params}) => {
     const { id } = params;
 
@@ -5,11 +7,15 @@ const Post = async ({params}) => {
     const data = await res.json();
 
   return (
-    <div>
-       Post
-        <h2>{data?.title}</h2>
-        <h3>{data?.body}</h3>
-        
+    <div className='bg-gray-100 p-3 min-h-[100vh]'>
+        <Link href={`/posts`}>
+            <button className="w-[200px] rounded-xl text-[20px] mb-3 py-2 px-4 text-white bg-gray-600 hover:bg-gray-900 hover:text-white hover:shadow-lg  duration-300 cursor-pointer">Back</button>    
+        </Link>
+        <div>
+        <p className="text-[18px]">Post {data?.id}</p>
+        <h2 className="text-[24px] font-bold my-2">{data?.title}</h2>
+        <h3 className="text-[18px]">{data?.body}</h3>
+        </div>
     </div>
   )
 }
